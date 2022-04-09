1b1e4633aaaf7510e348f40cce0aa0d849c9d484
Narendra8426 <narendranitjsr@gmail.com>
2022-04-09 16:15:33 +0530

    finally commited

---
+++ wcat.js
@@ -0,0 +1,8 @@
+// 1) node wcat.js filepath => displays the contents of a file in terminal
+// eg:- node wcat.js f1.txt(
+//const fs= require("fs");
+//console.log(fs);
+var a=6;
+var b= 12;
+console.log(a+b);
+console.log("i am going rock it");
